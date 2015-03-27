acf8e1383bfcd3d766dc3e74498c64730895b738
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2015-03-27 17:00:23 -0400

    removing dummy test

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
-VERSION = open(os.path.join(here, 'VERSION.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read()
 
 install_requires = [
     'pyramid',
--- test_fake.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class TestFakeness(unittest.TestCase):
-	def test_1_plus_1(self):
-		assert 1 + 1 == 2
