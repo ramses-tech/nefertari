620cfef72d4a2101faaf3e343d72c37414fecbca
Artem Kostiuk <postatum@gmail.com>
2015-04-20 16:39:35 +0300

    Refactor, bugfix ApiKey generation and ApiKeyAuthenticationPolicy

--- nefertari/authentication.py
+++ nefertari/authentication.py
@@ -1,13 +1,31 @@
-from zope.dottedname.resolve import resolve
+import uuid
+
 from pyramid.authentication import CallbackAuthenticationPolicy
 
 from nefertari import engine as eng
 
 
+def apikey_token():
+    """ Generate ApiKey.token using uuid library. """
+    return uuid.uuid4().hex.replace('-', '')
+
+
 def apikey_model(user_model):
-    model_name = user_model.__class__.__name__
+    """ Generate ApiKey model class and connect it with :user_model:.
+
+    ApiKey is generated having relationship to user model class :user_model:
+    and has One-to-One relationship with backreference.
+    ApiKey is setup to be auto-generated when new :user_model: is created.
+
+    Returns ApiKey document class. If ApiKey is already defined, it is not
+    generated again.
+
+    Arguments:
+        :user_model: Class that represents user model for which api keys will
+            be generated and with which ApiKey will have relationship.
+    """
     try:
-        return eng.get_document_cls(model_name)
+        return eng.get_document_cls('ApiKey')
     except ValueError:
         pass
 
@@ -16,34 +34,71 @@ def apikey_model(user_model):
     }
     if hasattr(user_model, '__tablename__'):
         fk_kwargs['ref_column'] = '.'.join([user_model.__tablename__, 'id'])
+        fk_kwargs['ref_column_type'] = eng.IdField
 
     class ApiKey(eng.BaseDocument):
+        __tablename__ = 'nefertari_apikey'
+
         id = eng.IdField(primary_key=True)
-        token = eng.StringField()
+        token = eng.StringField(default=apikey_token)
         user = eng.Relationship(
-            document=model_name,
-            backref_name='api_key', uselist=False)
-        owner_id = eng.ForeignKeyField(
-            ref_document=model_name,
-            ref_column_typ=eng.IdField,
+            document=user_model.__name__,
+            uselist=False,
+            backref_name='api_key',
+            backref_uselist=False)
+        user_id = eng.ForeignKeyField(
+            ref_document=user_model.__name__,
             **fk_kwargs)
 
-    # TODO: Connect signals to create ApiKey on User creation
+        def reset_token(self):
+            self.update({'token': apikey_token()})
+            return self.token
+
+    ApiKey.autogenerate_for(user_model, 'user')
+
+    return ApiKey
 
 
 class ApiKeyAuthenticationPolicy(CallbackAuthenticationPolicy):
+    """ ApiKey authentication policy.
 
-    def __init__(self, user_model, *args, **kwargs):
-        if isinstance(user_model, basestring):
-            user_model = resolve(user_model)
+    Relies of `Authorization` header being used on request, e.g.:
+        `Authorization: ApiKey username:token`
+    """
+    def __init__(self, user_model, check=None, credentials_callback=None):
+        """ Init the policy.
+
+        Arguments:
+            :user_model: String name or class of a User model for which ApiKey
+                model to be generated
+            :check: A callback passed the username, api_key and the request,
+                expected to return None if user doesn't exist or a sequence of
+                principal identifiers (possibly empty) if the user does exist.
+                If callback is None, the userid will be assumed to exist with
+                no principals. Optional.
+            :credentials_callback: A callback passed the userid, expected to
+                return tuple containing 2 elements: username and user's api key.
+                Is used to generate 'WWW-Authenticate' header with a value of
+                valid 'Authorization' request header that should be used to
+                perform requests.
+        """
         self.user_model = user_model
-        self.api_key_model = apikey_model(self.user_model)
-        super(ApiKeyAuthenticationPolicy, self).__init__(*args, **kwargs)
+        if isinstance(self.user_model, basestring):
+            self.user_model = eng.get_document_cls(self.user_model)
+        apikey_model(self.user_model)
+
+        self.check = check
+        self.credentials_callback = credentials_callback
+        super(ApiKeyAuthenticationPolicy, self).__init__()
 
     def remember(self, request, userid, **kw):
-        # Get user
-        api_key = 'ApiKey {}:{}'.format(username, token)
-        return [('WWW-Authenticate', api_key)]
+        """ Return 'WWW-Authenticate' header with a value that should be used
+        in 'Authorization' header.
+        """
+        if self.credentials_callback:
+            username, token = self.credentials_callback(userid, request)
+            api_key = 'ApiKey {}:{}'.format(username, token)
+            return [('WWW-Authenticate', api_key)]
 
     def forget(self, request):
         """ Returns challenge headers. This should be attached to a response
@@ -51,18 +106,21 @@ class ApiKeyAuthenticationPolicy(CallbackAuthenticationPolicy):
         return [('WWW-Authenticate', 'ApiKey realm="%s"' % self.realm)]
 
     def unauthenticated_userid(self, request):
-        """ The userid parsed from the ``Authorization`` request header."""
+        """ Username parsed from the ``Authorization`` request header."""
         credentials = self._get_credentials(request)
         if credentials:
             return credentials[0]
 
     def callback(self, username, request):
+        """ Having :username: return user's identifiers or None. """
         credentials = self._get_credentials(request)
         if credentials:
             username, api_key = credentials
-            return self.check(username, api_key, request)
+            if self.check:
+                return self.check(username, api_key, request)
 
     def _get_credentials(self, request):
+        """ Extract username and api key token from 'Authorization' header """
         authorization = request.headers.get('Authorization')
         if not authorization:
             return None
