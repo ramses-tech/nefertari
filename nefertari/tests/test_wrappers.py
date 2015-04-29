1ef0a8ccc214c085ced5e7915c51701b894a2ce4
Artem Kostiuk <postatum@gmail.com>
2015-04-29 17:42:10 +0300

    Add more tests

--- nefertari/tests/test_wrappers.py
+++ nefertari/tests/test_wrappers.py
@@ -2,32 +2,91 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from mock import Mock
+from mock import Mock, patch
 from pyramid.testing import DummyRequest
 
 from nefertari import wrappers
 
 
-class WrappersTest(unittest.TestCase):
+class TestWrappers(unittest.TestCase):
 
-    def test_obj2dict(self):
+    def test_issequence(self):
+        class A(object):
+            def __init__(self, *args):
+                for arg in args:
+                    setattr(self, arg, lambda x: x)
+
+        assert not wrappers.issequence(A('strip'))
+        assert not wrappers.issequence(A('foo'))
+        assert wrappers.issequence(A('__getitem__'))
+        assert wrappers.issequence(A('__iter__'))
+        assert wrappers.issequence(A('__iter__', 'foo'))
+        assert wrappers.issequence(A('__getitem__', 'foo'))
+
+    def test_wrap_me_init(self):
+        wrap = wrappers.wrap_me(before='foo', after=['bar'])
+        assert wrap.before == ['foo']
+        assert wrap.after == ['bar']
+
+        wrap = wrappers.wrap_me(after=['bar'])
+        assert wrap.before == []
+        assert wrap.after == ['bar']
+
+        wrap = wrappers.wrap_me()
+        assert wrap.before == []
+        assert wrap.after == []
+
+    def test_wrap_me_call(self):
+        meth = lambda x: x
+        wrap = wrappers.wrap_me(before=['foo'], after=['bar'])
+        assert not hasattr(meth, '_before_calls')
+        assert not hasattr(meth, '_after_calls')
+        wrap(meth)
+        assert meth._before_calls == ['foo']
+        assert meth._after_calls == ['bar']
+
+        wrap(meth)
+        assert meth._before_calls == ['foo', 'foo']
+        assert meth._after_calls == ['bar', 'bar']
+
+    def test_callable_base(self):
+        class A(wrappers.callable_base):
+            pass
+
+        obj1 = A(id=1)
+        obj2 = A(id=2)
+        assert obj1 == obj2
+        assert obj1.kwargs == {'id': 1}
+        assert obj2.kwargs == {'id': 2}
+
+    def test_obj2dict_dict_result(self):
+        assert wrappers.obj2dict(None)(result={'a': 'b'}) == {'a': 'b'}
+
+    def test_obj2dict_regular(self):
         result = Mock()
-        result.to_dict.return_value = dict(a=1)
+        result.to_dict.return_value = {'a': 1}
+        assert wrappers.obj2dict(None)(result=result) == {'a': 1}
 
-        res = wrappers.obj2dict(request=None)(result=result)
-        self.assertEqual(dict(a=1), res)
-
+    def test_obj2dict_list_from_todict(self):
+        result = Mock()
         result.to_dict.return_value = [dict(a=1), dict(b=2)]
-        self.assertEqual([dict(a=1), dict(b=2)],
-                         wrappers.obj2dict(request=None)(result=result))
+        self.assertEqual(
+            [dict(a=1), dict(b=2)],
+            wrappers.obj2dict(request=None)(result=result))
 
+    def test_obj2dict_nested(self):
         special = Mock()
         special.to_dict.return_value = {'special': 'dict'}
-        result = ['a', 'b', special]
-        self.assertEqual(['a', 'b', {'special': 'dict'}],
-                         wrappers.obj2dict(request=None)(result=result))
+        result = [special]
+        self.assertEqual(
+            [{'special': 'dict'}],
+            wrappers.obj2dict(request=None)(result=result))
 
-        self.assertEqual([], wrappers.obj2dict(request=None)(result=[]))
+    def test_obj2dict_other_type(self):
+        self.assertEqual(
+            'foo',
+            wrappers.obj2dict(request=None)(result='foo'))
+
 
     def test_add_meta(self):
         result = {'data': [{'id': 4}]}
