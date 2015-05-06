9ae5e1a8ccbd93120241d8b022046c09b2392fac
Artem Kostiuk <postatum@gmail.com>
2015-05-06 18:28:26 +0300

    Remove utils.requests import

--- nefertari/utils/__init__.py
+++ nefertari/utils/__init__.py
@@ -1,7 +1,5 @@
 from nefertari.utils.data import *
 from nefertari.utils.dictset import *
 from nefertari.utils.utils import *
-from nefertari.utils.request import *
 
-_requests = Requests
 _split = split_strip
