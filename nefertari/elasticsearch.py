7e33fd4598a09c4583e2abc7644fde9a597a6500
Artem Kostiuk <postatum@gmail.com>
2015-06-01 13:25:03 +0300

    Catch index error in ESHttpConnection

--- nefertari/elasticsearch.py
+++ nefertari/elasticsearch.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import json
 import logging
 from functools import partial
 
@@ -29,6 +30,23 @@ class IndexNotFoundException(Exception):
 
 
 class ESHttpConnection(elasticsearch.Urllib3HttpConnection):
+    def _catch_index_error(self, response):
+        """ Catch and raise index errors which are not critical and thus
+        not raised by elasticsearch-py.
+        """
+        code, headers, raw_data = response
+        if not raw_data:
+            return
+        data = json.loads(raw_data)
+        if not data or not data.get('errors'):
+            return
+        try:
+            error_dict = data['items'][0]['index']
+            message = error_dict['error']
+        except (KeyError, IndexError):
+            return
+        raise exception_response(400, detail=message)
+
     def perform_request(self, *args, **kw):
         try:
             if log.level == logging.DEBUG:
@@ -36,8 +54,7 @@ class ESHttpConnection(elasticsearch.Urllib3HttpConnection):
                 if len(msg) > 512:
                     msg = msg[:300] + '...TRUNCATED...' + msg[-212:]
                 log.debug(msg)
-
-            return super(ESHttpConnection, self).perform_request(*args, **kw)
+            resp = super(ESHttpConnection, self).perform_request(*args, **kw)
         except Exception as e:
             log.error(e.error)
             status_code = e.status_code
@@ -49,6 +66,9 @@ class ESHttpConnection(elasticsearch.Urllib3HttpConnection):
                 status_code,
                 detail='elasticsearch error.',
                 extra=dict(data=e))
+        else:
+            self._catch_index_error(resp)
+            return resp
 
 
 def includeme(config):
