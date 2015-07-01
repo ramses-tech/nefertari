c6526f3a1bef4e30a0eed0d07928ac6b6500a124
Artem Kostiuk <postatum@gmail.com>
2015-07-01 17:40:58 +0300

    Refactor poly view/acl. Work on ACLs generation

--- nefertari/polymorphic.py
+++ nefertari/polymorphic.py
@@ -1,3 +1,6 @@
+from pyramid.security import (
+    DENY_ALL, Allow, principals_allowed_by_permission)
+
 from nefertari.view import BaseView
 from nefertari.acl import BaseACL
 from nefertari.utils import dictset
@@ -10,12 +13,52 @@ def includeme(config):
              factory=PolymorphicACL)
 
 
-class PolymorphicACL(BaseACL):
+class PolymorphicHelperMixin(object):
+    def _get_collections(self):
+        """ Get names of collections from request. """
+        collections = self.request.matchdict['collections'].split('/')[0]
+        collections = [coll.strip() for coll in collections.split(',')]
+        return set(collections)
+
+    def _get_resources(self, collections):
+        """ Get resources that correspond to names from :collections: """
+        res_map = self.request.registry._resources_map
+        resources = [res for res in res_map.values()
+                     if res.collection_name in collections
+                     or res.member_name in collections]
+        resources = [res for res in resources if res]
+        return set(resources)
+
+
+class PolymorphicACL(PolymorphicHelperMixin, BaseACL):
     def __init__(self, request):
         super(PolymorphicACL, self).__init__(request)
+        self.set_collections_acls()
+
+    def _get_least_principals(self, resources):
+        factories = [res.view._factory for res in resources]
+        contexts = [factory(self.request) for factory in factories]
+        # TODO: Get resource with least permissions
+        # principals = principals_allowed_by_permission(ctx)
 
 
-class PolymorphicESView(BaseView):
+    def set_collections_acls(self):
+        collections = self._get_collections()
+        resources = self._get_resources(collections)
+        principals = self._get_least_principals(resources)
+        self.acl = (Allow, principals, 'index')
+        self.acl = DENY_ALL
+
+
+
+
+class PolymorphicESView(PolymorphicHelperMixin, BaseView):
+    """ Polymorphic ES collection read view.
+
+    Has default implementation of 'index' view method that supports
+    all the ES collection read actions(query, aggregation, etc.) across
+    multiple collections of ES-based documents.
+    """
     def __init__(self, *args, **kwargs):
         """ Init view and set fake `self.Model` so its __name__ would
         contain names of all requested collections.
@@ -24,28 +67,16 @@ class PolymorphicESView(BaseView):
         types = self.determine_types()
         self.Model = dictset({'__name__': ','.join(types)})
 
-    @staticmethod
-    def _get_collections(request):
-        """ Get names of collections from request. """
-        collections = request.matchdict['collections'].split('/')[0]
-        collections = [coll.strip() for coll in collections.split(',')]
-        return set(collections)
-
-    @staticmethod
-    def _get_resources(request, collections):
-        """ Get resources that correspond to names from :collections: """
-        res_map = request.registry._resources_map
-        resources = [res for res in res_map.values()
-                     if res.collection_name in collections
-                     or res.member_name in collections]
-        resources = [res for res in resources if res]
-        return resources
-
     def determine_types(self):
+        """ Determine ES type names from request data.
+
+        In particular `request.matchdict['collections']` is used to
+        determine types names. Its value is comma-separated sequence
+        of collection names under which views have been registered.
+        """
         from nefertari.elasticsearch import ES
-        collections = PolymorphicESView._get_collections(self.request)
-        resources = PolymorphicESView._get_resources(
-            self.request, collections)
+        collections = self._get_collections()
+        resources = self._get_resources(collections)
         models = set([res.view.Model for res in resources])
         es_models = [mdl for mdl in models if mdl
                      and getattr(mdl, '_index_enabled', False)]
