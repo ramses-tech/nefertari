31d232d95527285ffd536bb64f0f09af6e2c660e
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2015-07-17 00:37:20 -0400

    cleaning up scaffold

--- nefertari/scaffolds/__init__.py
+++ nefertari/scaffolds/__init__.py
@@ -19,7 +19,7 @@ class NefertariStarterTemplate(PyramidTemplate):
         (2) for MongoEngine/MongoDB?
 
         [default is '1']: """) or '1']
-        vars['random_string'] = binascii.hexlify(os.urandom(20))
+
         if vars['package'] == 'site':
             raise ValueError("""
                 "Site" is a reserved keyword in Python.
