6a1e30990476dd5e75020b05d5674d6b3fda2375
Artem Kostiuk <postatum@gmail.com>
2016-02-10 12:04:39 +0200

    Install testing reqs in scaffold

--- nefertari/scripts/scaffold_test.py
+++ nefertari/scripts/scaffold_test.py
@@ -24,24 +24,31 @@ class ScaffoldTestCommand(object):
                                       unzip_setuptools=True)
 
     def run_tests(self, scaff_name):  # pragma: no cover
-        # TODO: Switch to proper folder and install package
-        # TODO: How to install package in case of ramses if this
-        # nef script?
         # TODO: How to pass engine number to scaffold creation?
         proj_name = scaff_name.title()
         try:
             self.old_cwd = os.getcwd()
             self.directory = tempfile.mkdtemp()
             self.make_venv(self.directory)
-            here = os.path.abspath(os.path.dirname(__file__))
             py_bin = os.path.join(self.directory, 'bin', 'python')
+
+            # Install library in created env
+            here = os.path.abspath(os.path.dirname(__file__))
             os.chdir(os.path.dirname(os.path.dirname(here)))
             subprocess.check_call([py_bin, 'setup.py', 'develop'])
             os.chdir(self.directory)
+
+            # Create app from scaffold and install it
             subprocess.check_call(
                 ['bin/pcreate', '-s', scaff_name, proj_name])
             os.chdir(proj_name)
             subprocess.check_call([py_bin, 'setup.py', 'install'])
+
+            # Install test requirements
+            test_reqs = os.path.join('tests', 'requirements.txt')
+            if os.path.exists(test_reqs):
+                subprocess.check_call(['pip', 'install', '-r', test_reqs])
+
             # Run actual scaffold tests
             pytest.main()
         finally:
