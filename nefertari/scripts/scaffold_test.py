5d3f5ec24f7d2f439206cd645609008ec08d700d
Artem Kostiuk <postatum@gmail.com>
2016-02-12 13:18:44 +0200

    Fix scaffold_test script envs

--- nefertari/scripts/scaffold_test.py
+++ nefertari/scripts/scaffold_test.py
@@ -30,16 +30,18 @@ class ScaffoldTestCommand(object):
 
     def run_tests(self, scaff_name):  # pragma: no cover
         proj_name = scaff_name.title()
+        orig_path = os.environ.get('PATH', '')
         try:
             self.old_cwd = os.getcwd()
             self.directory = tempfile.mkdtemp()
             self.make_venv(self.directory)
-            py_bin = os.path.join(self.directory, 'bin', 'python')
+            os.environ['PATH'] = os.path.join(
+                self.directory, 'bin') + ':' + orig_path
 
             # Install library in created env
             here = os.path.abspath(os.path.dirname(__file__))
             os.chdir(os.path.dirname(os.path.dirname(here)))
-            check_call([py_bin, 'setup.py', 'develop'])
+            check_call(['python', 'setup.py', 'develop'])
             os.chdir(self.directory)
 
             # Create app from scaffold and install it
@@ -48,21 +50,17 @@ class ScaffoldTestCommand(object):
                 stdin=PIPE, stdout=PIPE)
             popen.communicate(ENGINE)
             os.chdir(proj_name)
-            check_call([py_bin, 'setup.py', 'install'])
+            check_call(['python', 'setup.py', 'install'])
 
             # Install test requirements
             test_reqs = os.path.join(scaff_name, 'tests', 'requirements.txt')
             if os.path.exists(test_reqs):
-                pip = os.path.join(self.directory, 'bin', 'pip')
-                check_call([pip, 'install', '-r', test_reqs])
+                check_call(['pip', 'install', '-r', test_reqs])
 
             # Run actual scaffold tests
-            # check_call([
-            #     os.path.join(self.directory, 'bin', 'py.test'),
-            #     scaff_name,
-            # ])
-            pytest.main()
+            check_call(['py.test', scaff_name])
         finally:
+            os.environ['PATH'] = orig_path
             shutil.rmtree(self.directory)
             os.chdir(self.old_cwd)
 
